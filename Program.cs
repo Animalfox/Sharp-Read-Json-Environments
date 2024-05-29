a4e4760b296462bda708173fd413ccf229470943
Animalfox <a72e@outlook.com>
2024-05-30 02:50:33 +0300

    Some Fixes

--- Program.cs
+++ Program.cs
@@ -4,7 +4,7 @@ var config = new ConfigurationBuilder()
     .AddJsonFile("app-settings.json")
     .AddEnvironmentVariables()
     .Build();
-Settings? settings = config.GetRequiredSection("Settings").Get<Settings>();
+var settings = config.GetRequiredSection("Settings").Get<Settings>();
 var message = settings?.Message;
 
 Console.WriteLine(""+message);
